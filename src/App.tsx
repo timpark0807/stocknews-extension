d133c716c240a04e6d104de6386f9083ea2db6b3
Tim Park <timpark0807@gmail.com>
2020-06-02 15:50:15 -0700

    Add a backbutton

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from "react-bootstrap/Navbar";
 import MainPage from "./MainPage";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-import { BrowserRouter } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar bg="primary" expand="lg" variant="dark">
-          <Navbar.Brand>Stock News</Navbar.Brand>
+
+          <Route exact path="/index.html">
+            <Navbar.Brand>Stock News</Navbar.Brand>
+          </Route>
+
+          <Route exact path="/news">
+            <Link to="/index.html">
+              <Button variant="secondary">Back</Button>
+            </Link>
+          </Route>
+
         </Navbar>
+
         <MainPage/>
       </BrowserRouter>
     </div>
--- src/NewsTable.tsx
+++ src/NewsTable.tsx
@@ -3,11 +3,13 @@ import Table from "react-bootstrap/Table";
 import { Articles } from "./MainPage"
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import { Container, Row, Col } from 'react-bootstrap';
 
 interface Props {
     news: Articles;
 }
 export const NewsTable: React.FC<Props> = ({ news }) => {
+
     const renderTable = () => {
         return news.articles.map(function(article) {
             return (
@@ -21,22 +23,26 @@ export const NewsTable: React.FC<Props> = ({ news }) => {
 
     return (
         <div>
-        <Link to="/index.html">
-            <Button variant="primary">Back</Button>
-        </Link>
-
         {news.articles.length > 1 && 
-            <Table style={{width: "100%"}}>
-            <thead>
-                <tr>
-                <th style={{width: "85%"}}>Headline</th>
-                <th style={{width: "15%"}}>Source</th>
-                </tr>
-            </thead>
-            <tbody>
-                {renderTable()}
-            </tbody>
-            </Table>
+            <div>
+                <Row>
+                    <Col style={{fontWeight:"bold", fontSize:"xx-large"}} xs={8}><label>${news.ticker}</label></Col>
+                </Row>
+                
+                <Row>
+                    <Table style={{width: "100%"}}>
+                    <thead>
+                        <tr>
+                        <th style={{width: "85%"}}>Headline</th>
+                        <th style={{width: "15%"}}>Source</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderTable()}
+                    </tbody>
+                    </Table>
+                </Row>
+            </div>
         }
         </div>
 
