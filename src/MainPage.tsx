623dfeb9e7e54fa5ca4170450abee4a9aa7f7db4
Tim Park <timpark0807@gmail.com>
2020-06-03 20:18:56 -0700

    Move all search logic from MainPage component to Search component for better modularization

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { NewsTable } from './NewsTable'
-import axios, {AxiosResponse} from 'axios';
 import { Search } from "./Search";
 import { Route } from "react-router-dom";
 import {NewsHeader} from './NewsHeader';
-import {FormControlProps} from "react-bootstrap"
+
 export interface Article {
     headline: string;
     source: string;
@@ -24,78 +23,36 @@ export interface Profile {
 
 export const MainPage: React.FC = () => {
 
-    const token = process.env.REACT_APP_api_token;
-    const [last, setLast] = useState(5)
+    const [last, setLast] = useState("5")
     const [ticker, setTicker] = useState("");
     const [articles, setArticles] = useState<Articles>({ticker:"", articles:[]});
     const [profile, setProfile] = useState<Profile>({companyName:"", latestPrice:"", symbol:""});
-
-    function handleClick(event: React.MouseEvent<HTMLButtonElement>){
-        axios.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/news?last=" + last + "&token=" + token)
-        .then(response => {
-            processArticles(response);
-            console.log(response);
-        })
-        .catch(error =>{
-            setArticles({ticker:ticker, articles:[]});
-        })
-
-        axios.get("https://cloud.iexapis.com/stable/stock/"+ticker +"/quote?token="+token)
-        .then(response => {
-            processProfile(response);
-        })
-        .catch(error =>{
-            setProfile({companyName:"", latestPrice:"", symbol:""});
-        })
-    };
-    
-    function processProfile(resp: AxiosResponse<any>) {
-        setProfile({ 
-            companyName: resp.data.companyName,
-            latestPrice: resp.data.latestPrice,
-            symbol: resp.data.symbol
-        });
-    };
-
-    function processArticles(resp: AxiosResponse<any> ) {
-        var arr: Article[] = []; 
-
-        for (var item of resp.data) {
-            arr.push({
-                headline: item.headline,
-                source: item.source, 
-                url: item.url
-            });
-        }
-        setArticles({ticker:ticker, articles:arr});
-    };
-
-    function handleChange(event: React.ChangeEvent<HTMLInputElement>){
-        setTicker(event.target.value.toUpperCase());
-    };
-
-    function handleLastChange(event: any) {
-        setLast(event.target.value.toString());
-    };
 
     return (
 
         <div className="page">
             <Route path="/index.html">
-                <Search ticker={ticker} handleChange={handleChange} handleClick={handleClick} handleLastChange={handleLastChange} />
+                <Search 
+                    last={last} 
+                    ticker={ticker} 
+                    setArticles={setArticles} 
+                    setProfile={setProfile} 
+                    setTicker={setTicker} 
+                    setLast={setLast}
+                />
             </Route>
 
             <Route exact path="/news">
                 {articles.articles.length > 1 ?
 
-                // if request was succesful, display news page. 
+                    // if request was succesful, display news page. 
                     <div> 
                         <NewsHeader profile={profile}/>
                         <NewsTable news={articles} profile={profile}/>
                     </div>
                 : 
 
-                // if request did not return a valid result, display error page. 
+                    // if request did not return a valid result, display error page. 
                     <div>
                         <label>Sorry! We couldn't find anything for ${ticker}.</label>
                         <br/>
--- src/Search.tsx
+++ src/Search.tsx
@@ -5,47 +5,101 @@ import FormControl from 'react-bootstrap/FormControl';
 import { Link } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
 import FormGroup from 'react-bootstrap/FormGroup';
+import axios, {AxiosResponse} from 'axios';
+import {Article, Articles, Profile} from "./MainPage"; 
 
 interface Props {
-    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    handleClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    handleLastChange: (event: any) => void;
+    setArticles: React.Dispatch<React.SetStateAction<Articles>>;
+    setTicker: React.Dispatch<React.SetStateAction<string>>;
+    setProfile: React.Dispatch<React.SetStateAction<Profile>>;
+    setLast: React.Dispatch<React.SetStateAction<string>>;
     ticker: string;
+    last: string;
 };
 
-export const Search: React.FC<Props> = ({handleChange, handleClick, handleLastChange, ticker }) => {
+export const Search: React.FC<Props> = ({setArticles, setTicker, setProfile, setLast, ticker, last }) => {
+
+    const token = process.env.REACT_APP_api_token;
+
+    function handleClick(event: React.MouseEvent<HTMLButtonElement>){
+        axios.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/news?last=" + last + "&token=" + token)
+        .then(response => {
+            processArticles(response);
+            console.log(response);
+        })
+        .catch(error =>{
+            setArticles({ticker:ticker, articles:[]});
+        })
+
+        axios.get("https://cloud.iexapis.com/stable/stock/"+ticker +"/quote?token="+token)
+        .then(response => {
+            processProfile(response);
+        })
+        .catch(error =>{
+            setProfile({companyName:"", latestPrice:"", symbol:""});
+        })
+    };
+    
+    function processProfile(resp: AxiosResponse<any>) {
+        setProfile({ 
+            companyName: resp.data.companyName,
+            latestPrice: resp.data.latestPrice,
+            symbol: resp.data.symbol
+        });
+    };
+
+    function processArticles(resp: AxiosResponse<any> ) {
+        var arr: Article[] = []; 
+
+        for (var item of resp.data) {
+            arr.push({
+                headline: item.headline,
+                source: item.source, 
+                url: item.url
+            });
+        }
+        setArticles({ticker:ticker, articles:arr});
+    };
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>){
+        setTicker(event.target.value.toUpperCase());
+    };
+
+    function handleLastChange(event: any) {
+        setLast(event.target.value.toString());
+    };
 
     return (
-    <div>
-        <label>Enter Ticker:</label>
-        <InputGroup className="mb-3" onChange={handleChange}>
-            <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-            placeholder="Ticker"
-            aria-label="Ticker"
-            aria-describedby="basic-addon1"
-            value={ticker}
-            /> 
-        </InputGroup>
-        <Form>
-            <FormGroup>
-                <Form.Label>Display Last: </Form.Label>
-                    <Form.Control as="select" onChange={handleLastChange}>
-                    <option>5</option>
-                    <option>10</option>
-                    <option>15</option>
-                    </Form.Control>
-            </FormGroup>
-        </Form>
+        <div>
+            <label>Enter Ticker:</label>
+            <InputGroup className="mb-3" onChange={handleChange}>
+                <InputGroup.Prepend>
+                    <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                placeholder="Ticker"
+                aria-label="Ticker"
+                aria-describedby="basic-addon1"
+                value={ticker}
+                /> 
+            </InputGroup>
+            <Form>
+                <FormGroup>
+                    <Form.Label>Display Last: </Form.Label>
+                        <Form.Control as="select" onChange={handleLastChange} value={last}>
+                        <option>5</option>
+                        <option>10</option>
+                        <option>15</option>
+                        </Form.Control>
+                </FormGroup>
+            </Form>
 
-        <div className="float-right mb-3">
-            <Link to="/news">
-                <Button variant="primary" onClick={handleClick}>Search</Button>
-            </Link>
+            <div className="float-right mb-3">
+                <Link to="/news">
+                    <Button variant="primary" onClick={handleClick}>Search</Button>
+                </Link>
+            </div>
         </div>
-    </div>
     )
 
 };
